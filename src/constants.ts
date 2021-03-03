2f6cde16ff86e2f3a3b02dbe30b9b8c9eacf8b96
lenconda <i@lenconda.top>
2021-03-03 21:45:26 +0800

    fix: user module global

---
+++ src/constants.ts
@@ -0,0 +1,5 @@
+// POST Body 中需要 email 字段
+export const ERR_BODY_EMAIL_REQUIRED = 'ERR_BODY_EMAIL_REQUIRED';
+
+// POST Body 中需要 password 字段
+export const ERR_BODY_PASSWORD_REQUIRED = 'ERR_BODY_PASSWORD_REQUIRED';
