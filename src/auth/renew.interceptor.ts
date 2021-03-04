ade2b55c695b6ea29d4711d6870f7e68d72b7a6d
lenconda <i@lenconda.top>
2021-03-04 23:55:37 +0800

    add: renew token before last minute of expiration

--- src/auth/renew.interceptor.ts
+++ src/auth/renew.interceptor.ts
@@ -6,8 +6,10 @@ import {
   Inject,
 } from '@nestjs/common';
 import { Observable } from 'rxjs';
-// import { map } from 'rxjs/operators';
+import { map } from 'rxjs/operators';
 import { AuthService } from './auth.service';
+import jwt from 'jsonwebtoken';
+import _ from 'lodash';
 
 export type Response = Record<string, any>;
 
@@ -22,12 +24,23 @@ export class RenewInterceptor<T> implements NestInterceptor<T, Response> {
     context: ExecutionContext,
     next: CallHandler,
   ): Observable<Response> {
-    return next.handle();
-    // return next.handle().pipe(
-    //   map((data) => ({
-    //     ...data,
-    //     token: 'fuck you',
-    //   })),
-    // );
+    const request = context.switchToHttp().getRequest();
+    const token = ((request?.headers?.authorization as string) || '').slice(7);
+    const user = request?.user || {};
+    const email = user?.email || '';
+    if (token && email) {
+      const payload = jwt.decode(token);
+      const { exp = Date.now() / 1000 } = payload as Record<string, any>;
+      const expireTimestamp = exp * 1000;
+      const additionalResponseData = {} as Record<string, any>;
+      if (expireTimestamp - Date.now() < 60000) {
+        additionalResponseData.token = this.authService.sign(email);
+      }
+      return next
+        .handle()
+        .pipe(map((data) => _.merge(data, additionalResponseData)));
+    } else {
+      return next.handle();
+    }
   }
 }
