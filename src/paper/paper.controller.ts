bf9bc0f21dfacb373399b280573e8cc00c080df5
lenconda <i@lenconda.top>
2021-03-11 12:08:07 +0800

    add: delete papers

--- src/paper/paper.controller.ts
+++ src/paper/paper.controller.ts
@@ -1,11 +1,42 @@
-import { Controller, Get, Param, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { CurrentUser } from 'src/user/user.decorator';
 import { PaperService } from './paper.service';
 
 @Controller('/api/paper')
 @UseGuards(AuthGuard('jwt'))
 export class PaperController {
   constructor(private readonly paperService: PaperService) {}
+
+  @Post()
+  async createPaper(
+    @CurrentUser() user,
+    @Body('title') title: string,
+    @Body('public') isPublic = false,
+  ) {
+    return await this.paperService.createPaper(user, title, isPublic);
+  }
+
+  @Delete()
+  async deletePapers(@CurrentUser() user, @Body('papers') paperIds: string[]) {
+    return this.paperService.deletePapers(
+      user,
+      paperIds.map((paperId) => parseInt(paperId)),
+    );
+  }
+
+  @Delete('/:paper')
+  async deletePaper(@CurrentUser() user, @Param('paper') paperId: string) {
+    return await this.paperService.deletePapers(user, [parseInt(paperId)]);
+  }
 
   @Get('/:paper')
   async getPaper(@Param('paper') paperId: string) {
--- src/paper/paper.service.ts
+++ src/paper/paper.service.ts
@@ -1,14 +1,57 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { Role } from 'src/role/role.entity';
+import { User } from 'src/user/user.entity';
+import { In, Repository } from 'typeorm';
 import { Paper } from './paper.entity';
+import { PaperUser } from './paper_user.entity';
 
 @Injectable()
 export class PaperService {
   constructor(
     @InjectRepository(Paper)
     private readonly paperRepository: Repository<Paper>,
+    @InjectRepository(PaperUser)
+    private readonly paperUserRepository: Repository<PaperUser>,
+    @InjectRepository(Role)
+    private readonly roleRepository: Repository<Role>,
   ) {}
+
+  async createPaper(creator: User, title: string, isPublic: boolean) {
+    const paper = this.paperRepository.create({
+      title,
+      public: isPublic,
+    });
+    await this.paperRepository.save(paper);
+    const paperUser = this.paperUserRepository.create({
+      paper,
+      user: creator,
+      role: {
+        id: 'resource/paper/owner',
+      },
+    });
+    await this.paperUserRepository.save(paperUser);
+    return paper;
+  }
+
+  async deletePapers(creator: User, paperIds: number[]) {
+    const paperUsers = await this.paperUserRepository.find({
+      where: {
+        paper: {
+          id: In(paperIds),
+        },
+        user: {
+          email: creator.email,
+        },
+        role: {
+          id: 'resource/paper/owner',
+        },
+      },
+      relations: ['paper'],
+    });
+    const papers = paperUsers.map((paperUser) => paperUser.paper);
+    await this.paperRepository.delete(papers.map((paper) => paper.id));
+  }
 
   async getPaper(paperId: number) {
     return this.paperRepository.findOne({
